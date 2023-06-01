ada5bab9d3393ec1121a94e4edc2de8917bb6db8
fahd <fahd@pop-os.localdomain>
2023-06-01 14:32:47 +0500

    second commit for testing

--- youtubeClone/src/App.jsx
+++ youtubeClone/src/App.jsx
@@ -1,15 +1,26 @@
-
-
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/joy";
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
 function App() {
-  
-
   return (
-    <>
-      <div>APPs</div>
-    </>
-  )
+    <BrowserRouter>
+      <Box sx={{ background: "#000" }}>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
